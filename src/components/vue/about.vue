a50f29bfa0716c3cdac3fe50e4e4103bc81e5faa
Christian Granado <xxamer_@hotmail.com>
2025-02-08 15:03:38 +0000

    Update some info

---
+++ src/components/vue/about.vue
@@ -0,0 +1,15 @@
+<template>
+  <div @click="changeVariable" class="alert">
+    This is made with Vue loading on astro
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from "vue";
+onMounted(() => {
+  console.log("Mounted");
+});
+const test = ref("test");
+const changeVariable = () => {
+  test.value = "hola";
+};
+</script>
